4d4242def5b9f387dde9b85eca1b6dc4171aab6d
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-19 17:26:31 +0300

    Решение задачи №6, Семинар 2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Write("Введите значение: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int Y = 0;
+int y = 0;
+int x = X;
+while (X != 0)
+{
+    Y++;
+    X /= 10;
+}
+//int a = Y - (Y - 3);
+if (Y > 2)
+{
+    while (x != 0)
+    {
+        y++;
+        x /= 10;
+        if (y == Y - (Y - (Y - 3)))
+        {
+            Console.WriteLine($"Третья цифра введённого числа: {x % 10}");
+
+        }
+    }
+}
+else
+{
+Console.WriteLine($"Третьей цифры нет");
+}
